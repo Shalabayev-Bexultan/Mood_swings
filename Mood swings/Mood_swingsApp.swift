2bef9d0e619a1829f934fefe97b07485768e0cf5
bexultan99 <21200122@life.hkbu.edu.hk>
2024-04-13 00:09:30 +0100

    Initial Commit

---
+++ Mood swings/Mood_swingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_swingsApp.swift
+//  Mood swings
+//
+//  Created by Bexultan Shalabayev on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Mood_swingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
